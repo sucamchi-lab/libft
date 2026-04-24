d373f65f4e5fc86405a4d7328f5c4138d3779f4f
scamlett <scamlett@student.42malaga.com>
2026-04-24 17:49:50 +0200

    corrijo calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: scamlett <scamlett@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/20 11:08:43 by scamlett          #+#    #+#             */
-/*   Updated: 2026/04/24 11:52:05 by scamlett         ###   ########.fr       */
+/*   Updated: 2026/04/24 17:49:40 by scamlett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,19 @@
 
 void	*ft_calloc(size_t count, size_t size)
 {
-	void	*ptr;
+	unsigned char	*ptr;
 
+	if (count == 0 || size == 0)
+		return (malloc(1));
+	if (size > ((size_t)-1) / count)
+		return (NULL);
 	ptr = malloc(count * size);
 	if (!ptr)
 		return (NULL);
-	ft_bzero(ptr, size * count);
+	ft_bzero(ptr, count * size);
 	return (ptr);
 }
+
 /*
 #include <stdio.h>
 
@@ -29,7 +34,7 @@ int	main(void)
 {
 	int	*arr;
 
-	arr = ft_calloc(4, sizeof(int));
+	arr = ft_calloc(0, 0);
 	if (!arr)
 		return (1);
 	printf("%d\n", arr[3]);
