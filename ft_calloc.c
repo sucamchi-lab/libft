c997d5d1cba03e0b8a6bbfae170a4333bbbac2e7
sucamchi-lab <sucamchi@gmail.com>
2026-04-25 16:25:13 +0200

    overflow safe

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,16 +6,26 @@
 /*   By: scamlett <scamlett@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/20 11:08:43 by scamlett          #+#    #+#             */
-/*   Updated: 2026/04/24 19:07:27 by scamlett         ###   ########.fr       */
+/*   Updated: 2026/04/25 16:21:11 by scamlett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdint.h>
 
+/*
+It is necessary to check for integer overflows in count * size
+before calling malloc. If the result overflows, malloc may allocate less memory
+than expected, leading to buffer overflows or memory leaks. This implementation
+uses (size_t)-1 to represent the maximum value of size_t for the check.
+This ensures that ft_calloc behaves safely and avoids undefined behavior.
+*/
 void	*ft_calloc(size_t count, size_t size)
 {
 	unsigned char	*ptr;
 
+	if (size != 0 && count > ((size_t)-1) / size)
+		return (NULL);
 	ptr = malloc(count * size);
 	if (!ptr)
 		return (NULL);
